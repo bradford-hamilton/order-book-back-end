85ebc764eb0c44d0c5bdff968c723759f7af8a69
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-07-08 21:39:41 -0600

    Remove unused http code

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-import createError from 'http-errors';
 import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import indexRouter from './src/routes/index';
 import { Server } from 'http';
+import OrderBooks from './src/lib/orderBooks';
 
 const app = express();
 const server = Server(app);
 const io = require('socket.io')(server);
-
-import OrderBooks from './src/lib/orderBooks';
 
 io.on('connection', (socket) => {
   let intervalId;
@@ -28,29 +22,6 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     clearInterval(intervalId);
   })
-});
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render error
-  res.status(err.status || 500);
-  res.json({ message: `Error: ${err}` });
 });
 
 server.listen(process.env.PORT || 4000);
